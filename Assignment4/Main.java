0696430a577f63491faf1fede9e510795c7c8bf7
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-29 21:44:43 +0200

    Done

--- Assignment4/Main.java
+++ Assignment4/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         ColorStore.createNewColor(0,0,0); // color already exist (from the basic colors)
-        ColorStore.createNewColor(4,4,4); // new color added
+        ColorStore.createNewColor(4,4,4); // new color added 
         ColorStore.printColors();
     }
 }
