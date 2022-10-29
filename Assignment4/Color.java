4a32a806b517b7ab501120da24a44750207f8481
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-29 21:44:08 +0200

    Done

--- Assignment4/Color.java
+++ Assignment4/Color.java
@@ -1,5 +1,6 @@
 public final class Color{
     private final int red, green, blue;
+    private final String key;
     public Color(int red, int green, int blue) throws Exception {
         if( red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255){
             throw new Exception("Invalid values");
@@ -7,19 +8,21 @@ public final class Color{
         this.red = red;
         this.green = green;
         this.blue = blue;
-        if(red+green+blue == 255 && red == 255)System.out.println("Red color");
-        else if(red+green+blue == 255 && green == 255)System.out.println("Green color");
-        else if(red+green+blue == 255 && blue == 255)System.out.println("Blue color");
-        else if(red+green+blue == 765)System.out.println("White color");
-        else if(red+green+blue == 0)System.out.println("Black color");
-        else System.out.println(this.getKey());
-    }
-    public static String getKey(int red, int green, int blue){
-        String str = red + " " + green + " " + blue;
-        return str;
+        this.key = this.red + " " + this.green + " " + this.blue;
     }
     public String getKey(){
-        String str = this.red + " " + this.green + " " + this.blue;
-        return str;
+        return key;
+    }
+    public static String checkKey(int red, int green, int blue){
+        return red + " " + green + " " + blue;
+    }
+    public void printColorName(){
+        int sum = this.red+this.green+this.blue;
+        if(sum == 255 && this.red == 255)System.out.println("Red color");
+        else if(sum == 255 && this.green == 255)System.out.println("Green color");
+        else if(sum == 255 && this.blue == 255)System.out.println("Blue color");
+        else if(sum == 765)System.out.println("White color");
+        else if(sum == 0)System.out.println("Black color");
+        else System.out.println(this.key);
     }
 }
